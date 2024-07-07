c4cbaf89a7e81abe4c7aef52d88546941174ae89
Bair <benedig@rambler.ru>
2024-07-07 16:46:14 +0900

    Добавлена пагинация и нумерация книг по возрастанию, а не по id

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -3,13 +3,19 @@
     <div>
         <div>
             @foreach($books as $book)
+                @php
+                    $index = ($books->currentPage() - 1) * $books->perPage() + $loop->iteration;
+                @endphp
                 <div>
                     <a href="{{ route('book.show', $book->id) }}"
                        class="text-decoration-none text-dark">
-                        {{ $loop->iteration }}. {{ $book->title }}
+                        {{ $index }}. {{ $book->title }}
                     </a>
                 </div>
             @endforeach
+            <div>
+                {{ $books->withQueryString()->links() }}
+            </div>
         </div>
     </div>
 @endsection
